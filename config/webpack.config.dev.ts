502b2ac91e33875bc18cfad7a7388b39c653781d
Zoe <lizhou1021@qq.com>
2021-01-05 22:45:11 +0800

    配置环境

---
+++ config/webpack.config.dev.ts
@@ -0,0 +1,45 @@
+import webpack from "webpack";
+import { Configuration } from "webpack-dev-server";
+import merge from "webpack-merge";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import path from "path";
+import commonConfig from "./webpack.config.common";
+
+const config: Configuration = merge(commonConfig, {
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    port: 5000,
+    compress: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                strictMath: false,
+                noIeCompat: true,
+                javascriptEnabled: true,
+                // modifyVars: theme,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin({}), new ReactRefreshWebpackPlugin({ overlay: false })],
+});
+
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Button, Intent } from "@blueprintjs/core";
+
+export default () => {
+    return (
+        <div>
+            <Button
+                text="text"
+                intent={Intent.SUCCESS}
+            />
+        </div>
+    );
+};
